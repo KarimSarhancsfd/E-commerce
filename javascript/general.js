39895b651f828d2e924bf76871137cc9eabcf32e
unknown <kimokomono74@gmail.com>
2025-02-12 21:18:07 +0200

    restructure coperate

--- javascript/general.js
+++ javascript/general.js
@@ -13,3 +13,21 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+// fetch("https://fakestoreapi.com/carts/user/2")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
+
+// fetch("https://fakestoreapi.com/carts", {
+//   method: "POST",
+//   body: JSON.stringify({
+//     userId: 5,
+//     date: 2020 - 02 - 03,
+//     products: [
+//       { productId: 5, quantity: 1 },
+//       { productId: 1, quantity: 5 },
+//     ],
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
