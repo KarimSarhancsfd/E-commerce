9bb6bc624eb65f7299cd19b6f4c98f57d49c4caa
unknown <kimokomono74@gmail.com>
2025-02-13 07:54:49 +0200

    logingin and regerstration page

--- javascript/general.js
+++ javascript/general.js
@@ -1,16 +1,24 @@
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
-
+const signin = document.getElementById("Sign in");
+//open side navebar
 if (bar) {
   bar.addEventListener("click", () => {
     nav.classList.add("active");
   });
 }
-
+//close side navbar
 if (close) {
   close.addEventListener("click", () => {
     nav.classList.remove("active");
+  });
+}
+
+//open signin page
+if (signin) {
+  signin.addEventListener("click", () => {
+    window.location.href = "signin.html";
   });
 }
 
