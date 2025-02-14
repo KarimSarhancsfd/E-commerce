65554fb1d50a06fc0768c39cacf600a34ee13b5a
unknown <kimokomono74@gmail.com>
2025-02-14 13:05:02 +0200

    login and regestration page restyling

--- javascript/general.js
+++ javascript/general.js
@@ -1,7 +1,7 @@
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
-const signin = document.getElementById("Sign in");
+const signin = document.getElementById("Log In");
 //open side navebar
 if (bar) {
   bar.addEventListener("click", () => {
@@ -18,24 +18,6 @@ if (close) {
 //open signin page
 if (signin) {
   signin.addEventListener("click", () => {
-    window.location.href = "signin.html";
+    window.location.href = "login.html";
   });
 }
-
-// fetch("https://fakestoreapi.com/carts/user/2")
-//   .then((res) => res.json())
-//   .then((json) => console.log(json));
-
-// fetch("https://fakestoreapi.com/carts", {
-//   method: "POST",
-//   body: JSON.stringify({
-//     userId: 5,
-//     date: 2020 - 02 - 03,
-//     products: [
-//       { productId: 5, quantity: 1 },
-//       { productId: 1, quantity: 5 },
-//     ],
-//   }),
-// })
-//   .then((res) => res.json())
-//   .then((json) => console.log(json));
