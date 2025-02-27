ff3a5b0a8a5b20c9680bcf65396cc5ea79cff4b1
unknown <kimokomono74@gmail.com>
2025-02-27 12:32:56 +0200

    shop page optimization

--- javascript/shopping-cart.js
+++ javascript/shopping-cart.js
@@ -21,29 +21,29 @@
 
 //here we are fetching data from an object
 
-fetch("https://fakestoreapi.com/products/1")
-  .then((res) => {
-    //res here is a varaiable
-    //   console.log(res);
-    return res.json();
-  })
-  .then((data_Category) => {
-    console.log(data);
-    let category = `<span>Category:${data_Category.category}</span>`;
-    document.querySelectorAll(".section-p1 span").forEach((span) => {
-      span.insertAdjacentHTML("beforeend", category);
-    });
-  })
-  .catch((error) => console.log(error));
+// fetch("https://fakestoreapi.com/products/1")
+//   .then((res) => {
+//     //res here is a varaiable
+//     //   console.log(res);
+//     return res.json();
+//   })
+//   .then((data_Category) => {
+//     console.log(data);
+//     let category = `<span>Category:${data_Category.category}</span>`;
+//     document.querySelectorAll(".section-p1 span").forEach((span) => {
+//       span.insertAdjacentHTML("beforeend", category);
+//     });
+//   })
+//   .catch((error) => console.log(error));
 
-fetch("https://fakestoreapi.com/products/1")
-  .then((res) => {
-    return res.json();
-  })
-  .then((data_price) => {
-    console.log(data_price);
-    let price = `<span>Price:${data_price.price}<span>`;
-    document.querySelectorAll(".section-p1 h4").forEach((h4) => {
-      h4.insertAdjacentHTML("beforeend", price);
-    });
-  });
+// fetch("https://fakestoreapi.com/products/1")
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data_price) => {
+//     console.log(data_price);
+//     let price = `<span>Price:${data_price.price}<span>`;
+//     document.querySelectorAll(".section-p1 h4").forEach((h4) => {
+//       h4.insertAdjacentHTML("beforeend", price);
+//     });
+//   });
